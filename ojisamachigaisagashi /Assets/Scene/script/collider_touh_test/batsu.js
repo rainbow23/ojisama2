201596bb7adfdb8c7226f9184560db18c7af65b3
rainbow23 <chicago_bulls_eye@hotmail.co.jp>
2012-11-16 21:54:50 +0900

    collider 判定　修正

---
+++ ojisamachigaisagashi /Assets/Scene/script/collider_touh_test/batsu.js
@@ -0,0 +1,12 @@
+#pragma strict
+
+function On_SimpleTap(gesture:Gesture){
+print("batsu");
+}
+function Start () {
+
+}
+
+function Update () {
+
+}
---
+++ ojisamachigaisagashi /Assets/Scene/script/collider_touh_test/maru.js
@@ -0,0 +1,11 @@
+#pragma strict
+function On_SimpleTap(gesture:Gesture){
+print("maru");
+}
+function Start () {
+
+}
+
+function Update () {
+
+}
